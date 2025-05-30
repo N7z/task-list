1fefb2de834908699a2167902d7b11b2ab51f83a
N7z <pejotinhas@gmail.com>
2025-05-30 14:16:16 -0300

    Minor style update on checkbox

--- resources/views/livewire/tasklist.blade.php
+++ resources/views/livewire/tasklist.blade.php
@@ -97,7 +97,7 @@ new class extends Component {
             <li class="flex justify-between items-center px-4 py-3 rounded-xl bg-zinc-100 dark:bg-zinc-700 shadow-sm">
                 <div class="flex items-center gap-3">
                     <div class="flex items-center">
-                        <input id="default-checkbox" type="checkbox" wire:click="toggle({{ $task->id }})" @checked($task->completed) class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <input id="default-checkbox" type="checkbox" wire:click="toggle({{ $task->id }})" @checked($task->completed) class="w-4 h-4 text-blue-600 bg-gray-100 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700">
                         <label for="default-checkbox" class="{{ $task->completed ? 'line-through' : '' }} ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $task->title }} — por {{ $task->user->name }}</label>
                     </div>
                 </div>
